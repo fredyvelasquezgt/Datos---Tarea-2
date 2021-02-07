d121866e22831e717c5563ba79dc116605167927
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2021-02-06 18:52:54 -0600

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Fredy Velasquez <macacoloco696gmail.com>
+ */
+public interface calculadora<E> {
+    public int suma(int x, int y);
+    public int resta(int x, int y);
+    public int multiplicacion(int x, int y);
+    public int division(int x, int y);
+    public int operar(Stack x);//operar recibe un stack en formato postfix, y lo opera para devolver un entero que es igual al valor total de la operación
+    public String decode(String a);//función que se encarga de leer el archivo y decodificar cada línea para realizar las operaciones de cada una
+}
